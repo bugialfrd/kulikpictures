7e7f7bb2cd398ecc515fa322a453635d5c61efcd
unknown <bugialfaridzi@gmail.com>
2024-08-08 10:07:51 +0700

    'kulikpictures'

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,6 +1,6 @@
 // This example uses `@web3forms/react` plugin and tailwindcss for css styling
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import useWeb3Forms from "@web3forms/react";
 
@@ -9,17 +9,14 @@ export default function Contact() {
     register,
     handleSubmit,
     reset,
-    watch,
-    control,
-    setValue,
     formState: { errors, isSubmitSuccessful, isSubmitting },
   } = useForm({
     mode: "onTouched",
   });
   const [isSuccess, setIsSuccess] = useState(false);
-  const [message, setMessage] = useState(false);
+  const [message, setMessage] = useState("");
 
-  // Please update the Access Key in the .env
+  // Replace with your actual API key or load from environment variables
   const apiKey = process.env.PUBLIC_ACCESS_KEY || "YOUR_ACCESS_KEY_HERE";
 
   const { submit: onSubmit } = useWeb3Forms({
@@ -40,24 +37,25 @@ export default function Contact() {
   });
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} className="my-10">
+    <div className="max-w-lg mx-auto mt-10">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="checkbox"
-          id=""
+          id="botcheck"
           className="hidden"
           style={{ display: "none" }}
-          {...register("botcheck")}></input>
+          {...register("botcheck")}
+        />
 
         <div className="mb-5">
           <input
             type="text"
             placeholder="Full Name"
-            autoComplete="false"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+            autoComplete="off"
+            className={`w-full px-4 py-3 border-2 rounded-md outline-none ${
               errors.name
-                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                ? "border-red-600 focus:border-red-600"
+                : "border-gray-300 focus:border-gray-600"
             }`}
             {...register("name", {
               required: "Full name is required",
@@ -72,19 +70,14 @@ export default function Contact() {
         </div>
 
         <div className="mb-5">
-          <label htmlFor="email_address" className="sr-only">
-            Email Address
-          </label>
           <input
-            id="email_address"
             type="email"
             placeholder="Email Address"
-            name="email"
-            autoComplete="false"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white rounded-md outline-none dark:placeholder:text-gray-200 dark:bg-gray-900   focus:ring-4  ${
+            autoComplete="off"
+            className={`w-full px-4 py-3 border-2 rounded-md outline-none ${
               errors.email
-                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                ? "border-red-600 focus:border-red-600"
+                : "border-gray-300 focus:border-gray-600"
             }`}
             {...register("email", {
               required: "Enter your email",
@@ -101,14 +94,13 @@ export default function Contact() {
           )}
         </div>
 
-        <div className="mb-3">
+        <div className="mb-5">
           <textarea
-            name="message"
             placeholder="Your Message"
-            className={`w-full px-4 py-3 border-2 placeholder:text-gray-800 dark:text-white dark:placeholder:text-gray-200 dark:bg-gray-900   rounded-md outline-none  h-36 focus:ring-4  ${
+            className={`w-full px-4 py-3 border-2 rounded-md outline-none ${
               errors.message
-                ? "border-red-600 focus:border-red-600 ring-red-100 dark:ring-0"
-                : "border-gray-300 focus:border-gray-600 ring-gray-100 dark:border-gray-600 dark:focus:border-white dark:ring-0"
+                ? "border-red-600 focus:border-red-600"
+                : "border-gray-300 focus:border-gray-600"
             }`}
             {...register("message", {
               required: "Enter your Message",
@@ -116,7 +108,6 @@ export default function Contact() {
           />
           {errors.message && (
             <div className="mt-1 text-red-600">
-              {" "}
               <small>{errors.message.message}</small>
             </div>
           )}
@@ -124,24 +115,31 @@ export default function Contact() {
 
         <button
           type="submit"
-          className="w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-gray-200 px-7 dark:bg-white dark:text-black ">
+          className={`w-full py-4 font-semibold text-white transition-colors bg-gray-900 rounded-md hover:bg-gray-800 focus:outline-none ${
+            isSubmitting ? "opacity-75 cursor-not-allowed" : ""
+          }`}
+          disabled={isSubmitting}
+        >
           {isSubmitting ? (
             <svg
-              className="w-5 h-5 mx-auto text-white dark:text-black animate-spin"
+              className="w-5 h-5 mr-3 animate-spin"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 24 24">
+              viewBox="0 0 24 24"
+            >
               <circle
                 className="opacity-25"
                 cx="12"
                 cy="12"
                 r="10"
                 stroke="currentColor"
-                strokeWidth="4"></circle>
+                strokeWidth="4"
+              ></circle>
               <path
                 className="opacity-75"
                 fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
             </svg>
           ) : (
             "Send Message"
@@ -150,16 +148,16 @@ export default function Contact() {
       </form>
 
       {isSubmitSuccessful && isSuccess && (
-        <div className="mt-3 text-sm text-center text-green-500">
+        <div className="mt-4 text-sm text-center text-green-500">
           {message || "Success. Message sent successfully"}
         </div>
       )}
       {isSubmitSuccessful && !isSuccess && (
-        <div className="mt-3 text-sm text-center text-red-500">
+        <div className="mt-4 text-sm text-center text-red-500">
           {message || "Something went wrong. Please try later."}
         </div>
       )}
-    </>
+    </div>
   );
 }
 
